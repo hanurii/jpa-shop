eb9d21fed1af673dbe6bcd64d13deca1167e1a81
han95210 <hanul95210@gmail.com>
2020-07-13 23:23:17 +0900

    도메인 모델과 테이블 설계 및 엔티티 클래스 개발

---
+++ src/main/java/com/inflearn/jpashop/domain/DeliveryStatus.java
@@ -0,0 +1,5 @@
+package com.inflearn.jpashop.domain;
+
+public enum DeliveryStatus {
+  READY, COMP
+}
---
+++ src/main/java/com/inflearn/jpashop/domain/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.inflearn.jpashop.domain;
+
+public enum OrderStatus {
+  ORDER, CANCEL
+}
