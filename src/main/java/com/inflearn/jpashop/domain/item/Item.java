dd4b2325b67d6e2f469d2e53bcc7f6b813958b6b
Hamill210 <han9521@naver.com>
2020-07-14 16:39:14 +0900

    Item 엔티티에 비즈니스 로직 추가

--- src/main/java/com/inflearn/jpashop/domain/item/Item.java
+++ src/main/java/com/inflearn/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package com.inflearn.jpashop.domain.item;
 
 import com.inflearn.jpashop.domain.Category;
+import com.inflearn.jpashop.exception.NotEnoughStockException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Column;
@@ -31,4 +32,24 @@ public abstract class Item {
 
   @ManyToMany(mappedBy = "items")
   private List<Category> categories = new ArrayList<>();
+
+  //==비즈니스 로직==//
+
+  /**
+   * stock 증가
+   */
+  public void addStock(int quantity) {
+    this.stockQuantity += quantity;
+  }
+
+  /**
+   * stock 감소
+   */
+  public void removeStock(int quantity) {
+    int restStock = this.stockQuantity - quantity;
+    if (restStock < 0) {
+      throw new NotEnoughStockException("need more stock");
+    }
+    this.stockQuantity = restStock;
+  }
 }
