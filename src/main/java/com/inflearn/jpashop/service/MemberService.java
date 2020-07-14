c7dc01bc6bc81dd6bbac46dfb4d1d1cc152aefe2
Hamill210 <han9521@naver.com>
2020-07-14 16:16:28 +0900

    회원 Service 개발

---
+++ src/main/java/com/inflearn/jpashop/service/MemberService.java
@@ -0,0 +1,43 @@
+package com.inflearn.jpashop.service;
+
+import com.inflearn.jpashop.domain.Member;
+import com.inflearn.jpashop.repository.MemberRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+  private final MemberRepository memberRepository;
+
+  /**
+   * 회원 가입
+   */
+  @Transactional
+  public Long join(Member member) {
+
+    validateDuplicateMember(member); //중복 회원 검증
+    memberRepository.save(member);
+    return member.getId();
+  }
+
+  private void validateDuplicateMember(Member member) {
+    List<Member> findMembers = memberRepository.findByName(member.getName());
+    if (!findMembers.isEmpty()) {
+      throw new IllegalStateException("이미 존재하는 회원입니다.");
+    }
+  }
+
+  //회원 전체 조회
+  public List<Member> findMembers() {
+    return memberRepository.findAll();
+  }
+
+  public Member findOne(Long memberId) {
+    return memberRepository.findOne(memberId);
+  }
+}
