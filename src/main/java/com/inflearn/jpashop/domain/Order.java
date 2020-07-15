b7943157bd8d1f2e016b9c4254a98f72fea78114
Hamill210 <han9521@naver.com>
2020-07-15 12:22:35 +0900

    주문, 주문상품 엔티티 개발

--- src/main/java/com/inflearn/jpashop/domain/Order.java
+++ src/main/java/com/inflearn/jpashop/domain/Order.java
@@ -18,15 +18,19 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "orders")
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order {
 
-  @Id @GeneratedValue
+  @Id
+  @GeneratedValue
   @Column(name = "order_id")
   private Long id;
 
@@ -47,7 +51,7 @@ public class Order {
   private OrderStatus status; //주문상태 [ORDER, CANCEL]
 
   //==연관관계 편의 메서드==//
-  public  void setMember(Member member) {
+  public void setMember(Member member) {
     this.member = member;
     member.getOrders().add(this);
   }
@@ -60,5 +64,47 @@ public class Order {
   public void setDelivery(Delivery delivery) {
     this.delivery = delivery;
     delivery.setOrder(this);
+  }
+
+  //==생성 메서드==//
+  public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+    Order order = new Order();
+    order.setMember(member);
+    order.setDelivery(delivery);
+    for (OrderItem orderItem : orderItems) {
+      order.addOrderItem(orderItem);
+    }
+    order.setStatus(OrderStatus.ORDER);
+    order.setOrderDate(LocalDateTime.now());
+    return order;
+  }
+
+  //==비즈니스 로직==//
+
+  /**
+   * 주문 취소
+   */
+  public void cancel() {
+    if (delivery.getStatus() == DeliveryStatus.COMP) {
+      throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+    }
+
+    this.setStatus(OrderStatus.CANCEL);
+    for (OrderItem orderItem : orderItems) {
+      orderItem.cancel();
+    }
+  }
+
+  //==조회 로직==//
+
+  /**
+   * 전체 주문 가격 조회
+   */
+  public int getTotalPrice() {
+    int totalPrice = 0;
+    for (OrderItem orderItem : orderItems) {
+      totalPrice += orderItem.getTotalPrice();
+    }
+    return totalPrice;
   }
 }
